3a83f4652c1bff3c43d1efb3c0e8249b7e707ccd
mmkgit <64713244+mmkgit@users.noreply.github.com>
2020-10-04 13:00:07 -0700

    Update TestScript.java

--- TestScript.java
+++ TestScript.java
@@ -2,6 +2,7 @@ public class TestScript{
 	
 	public static void main(String[] args){
 	System.out.println("hello, world!" + " should be conflict");
+	System.out.println("Update for Jenkins");
 	}
   
   public void display(){
